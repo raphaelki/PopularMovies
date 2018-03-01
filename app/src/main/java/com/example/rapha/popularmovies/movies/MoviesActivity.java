5401e90e69597326bb6ebeff9031b320d8fc1006
raphaki <raphael.kirchner@yahoo.de>
2018-03-01 18:41:50 +0100

    Show fragment in activity and move Background Task to fragment

--- app/src/main/java/com/example/rapha/popularmovies/movies/MoviesActivity.java
+++ app/src/main/java/com/example/rapha/popularmovies/movies/MoviesActivity.java
@@ -1,19 +1,10 @@
 package com.example.rapha.popularmovies.movies;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.example.rapha.popularmovies.R;
-import com.example.rapha.popularmovies.data.Movie;
-import com.example.rapha.popularmovies.utils.MovieDbJsonUtils;
-import com.example.rapha.popularmovies.utils.MovieDbNetworkUtils;
-
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.util.List;
 
 public class MoviesActivity extends AppCompatActivity {
 
@@ -22,38 +13,12 @@ public class MoviesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_overview);
-        new MovieDbQueryTask().execute(getString(R.string.api_key));
+        setContentView(R.layout.activity_movies);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MoviesFragment fragment = new MoviesFragment();
+        fragmentManager.beginTransaction().add(R.id.movies_fragment_frame, fragment).commit();
     }
 
-    class MovieDbQueryTask extends AsyncTask<String, Void, List<Movie>> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(List<Movie> movies) {
-            for (Movie movie : movies) {
-                Log.d(TAG, movie.getTitle());
-            }
-        }
-
-        @Override
-        protected List<Movie> doInBackground(String... strings) {
-            String apiKey = strings[0];
-            try {
-                String jsonData = MovieDbNetworkUtils.fetchPopularMovies(apiKey);
-                try {
-                    return MovieDbJsonUtils.parseMovieDbJson(jsonData);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
 }
--- app/src/main/java/com/example/rapha/popularmovies/movies/MoviesFragment.java
+++ app/src/main/java/com/example/rapha/popularmovies/movies/MoviesFragment.java
@@ -1,14 +1,28 @@
 package com.example.rapha.popularmovies.movies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.rapha.popularmovies.R;
+import com.example.rapha.popularmovies.data.Movie;
+import com.example.rapha.popularmovies.utils.MovieDbJsonUtils;
+import com.example.rapha.popularmovies.utils.MovieDbNetworkUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+
 public class MoviesFragment extends Fragment {
+
+    private final String TAG = getClass().getSimpleName();
 
     public MoviesFragment() {
     }
@@ -16,6 +30,42 @@ public class MoviesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_movies, container, false);
+
+        new MovieDbQueryTask().execute(getString(R.string.api_key));
+
+        return view;
+    }
+
+    class MovieDbQueryTask extends AsyncTask<String, Void, List<Movie>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+            for (Movie movie : movies) {
+                Log.d(TAG, movie.getTitle());
+            }
+        }
+
+        @Override
+        protected List<Movie> doInBackground(String... strings) {
+            String apiKey = strings[0];
+            try {
+                String jsonData = MovieDbNetworkUtils.fetchPopularMovies(apiKey);
+                try {
+                    return MovieDbJsonUtils.parseMovieDbJson(jsonData);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
